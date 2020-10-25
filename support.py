9c0f7618bcd1dd85a69f0c070ef4aaf188a69234
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-26 01:24:38 +0800

    Refactored and added DOCSTRINGS

--- support.py
+++ support.py
@@ -8,6 +8,12 @@ class Hangman:
 
         # Word dict
         def _word_to_guess(word):
+            """
+            This function extracts letters from the word to guess.
+            Letters are mapped to a dictionary.
+            :param word: Word to guess
+            :return: Dictionary of letters in the word to guess
+            """
             word_dict = {}
             for index, letter in enumerate(word):
                 if letter == ' ':
@@ -23,104 +29,117 @@ class Hangman:
 
         # Hangman
         self.hangman = [
-                        "|---------------\n"
-                        "|\n"
-                        "|\n"
-                        "|\n"
-                        "|\n"
-                        "|\n"
-                        "|_______________",
-                        "|---------------\n"
-                        "|       |\n"
-                        "|\n"
-                        "|\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (\n"
-                        "|\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_\n"
-                        "|\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|       |\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|       |\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|      /|\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|      /|\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|      /|\\\n"
-                        "|\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|      /|\\\n"
+            "|\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|      /|\\\n"
-                        "|       |\n" 
-                        "|\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|      /|\\\n"
+            "|       |\n"
+            "|\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|      /|\\\n"
-                        "|       |\n" 
-                        "|      /\n"
-                        "|_______________",
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|      /|\\\n"
+            "|       |\n"
+            "|      /\n"
+            "|_______________",
 
-                        "|---------------\n"
-                        "|       |\n"
-                        "|      (_)\n"
-                        "|      /|\\\n"
-                        "|       |\n" 
-                        "|      / \\\n"
-                        "|_______________"
-                    ]
+            "|---------------\n"
+            "|       |\n"
+            "|      (_)\n"
+            "|      /|\\\n"
+            "|       |\n"
+            "|      / \\\n"
+            "|_______________"
+        ]
 
         # Error count
         self.error_counter = 0
 
     def display_guessing_slate(self):
+        """
+        This function displays the current guessing slate.
+        :return: None
+        """
         print(''.join(self.guessing_slate))
 
     def display_hangman(self):
+        """
+        This function displays the current hangman state.
+        :return: None
+        """
         print(self.hangman[self.error_counter])
 
     def letter_validator(self, letter):
+        """
+        This function valiates the user's letter input.
+        :param letter: User's guess
+        :return: Respective functions
+        """
         if letter in self.word_dict.keys():
             for index in self.word_dict[letter]:
                 self.guessing_slate = self.guessing_slate[:index] + [letter] + self.guessing_slate[index + 1:]
